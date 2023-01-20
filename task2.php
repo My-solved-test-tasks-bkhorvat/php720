7c428a038bd6be6fb172dc0cba44df5a24e4a348
bkhorvat <bkhorvat.php@gmail.com>
2023-01-20 06:40:14 +0100

    task 2

---
+++ task2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+    You need to develop a program that would count the number of occurrences of some digit of your choice in
+    the number of your choice.  For example: the number 5 in the number 442158755745 occurs 4 times
+*/
+
+$number = "15253545";
+$checkNumber = '5';
+$count = 0;
+
+for ($i = 0; $i < strlen($number); $i++) {
+    if ($number[$i] == $checkNumber) {
+        $count++;
+    }
+}
+
+echo $count;
